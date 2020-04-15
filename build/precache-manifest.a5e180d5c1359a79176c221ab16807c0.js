857aea18ec4b7b7c996c5f7aff120133484a1fcb
Nahtaniel Woodbury <nxte01@gmail.com>
2020-04-14 20:30:39 -0700

    Fix

---
+++ build/precache-manifest.a5e180d5c1359a79176c221ab16807c0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a16ae76b17ee9f2c40f05a7796a25dab",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ff2bc4e57463811c5f53",
+    "url": "/static/css/main.e7477074.chunk.css"
+  },
+  {
+    "revision": "17813de5b501d6555c2b",
+    "url": "/static/js/2.5598b70a.chunk.js"
+  },
+  {
+    "revision": "ff2bc4e57463811c5f53",
+    "url": "/static/js/main.2effe486.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5044823b521b725f7c6ea8d32e336028",
+    "url": "/static/media/spotify.5044823b.png"
+  }
+]);
